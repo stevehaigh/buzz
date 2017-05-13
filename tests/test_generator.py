b1c7fb4dd5360c5b323dfb11bea55a7888e21219
Steve Haigh <steve.haigh@gmail.com>
2017-05-13 18:28:08 +0100

    fixing the build

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -17,6 +17,3 @@ def test_sample_multiple_words():
 def test_generate_buzz_of_at_least_five_words():
     phrase = buzz.generate_buzz()
     assert len(phrase.split()) >= 5
-
-def test_break_the_build():
-    assert False
