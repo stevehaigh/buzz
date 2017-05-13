10c266f136df415778e015301bfeec2cc1d0673b
Steve Haigh <steve.haigh@gmail.com>
2017-05-13 18:04:30 +0100

    Created

---
+++ buzz/buzz.py
@@ -0,0 +1,29 @@
+"""
+Buzzowrd tool from:
+    https://medium.com/bettercode/how-to-build-a-modern-ci-cd-pipeline-5faa01891a5b
+"""
+import random
+
+buzz = ('continuous testing', 'continuous integration',
+    'continuous deployment', 'continuous improvement', 'devops', 'micro services', 'serverless',
+    'high-scale')
+adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end', 'auto-scaling', )
+adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
+    'seriously', 'rapidly', 'autonomously', 'magicaly')
+verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts', 'delivers'
+    'integrates', 'deploys', 'reduces')
+
+def sample(l, n=1):
+    result = random.sample(l, n)
+    if n == 1:
+        return result[0]
+    return result
+
+def generate_buzz():
+    buzz_terms = sample(buzz, 2)
+    phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs),
+        sample(verbs), buzz_terms[1]])
+    return phrase.title()
+
+if __name__ == "__main__":
+    print generate_buzz()
---
+++ tests/test_generator.py
@@ -0,0 +1,20 @@
+import unittest
+
+from buzz import buzz
+
+def test_sample_single_word():
+    l = ('foo', 'bar', 'foobar')
+    word = buzz.sample(l)
+    assert word in l
+
+def test_sample_multiple_words():
+    l = ('foo', 'bar', 'foobar')
+    words = buzz.sample(l, 2)
+    assert len(words) == 2
+    assert words[0] in l
+    assert words[1] in l
+    assert words[0] is not words[1]
+
+def test_generate_buzz_of_at_least_five_words():
+    phrase = buzz.generate_buzz()
+    assert len(phrase.split()) >= 5
