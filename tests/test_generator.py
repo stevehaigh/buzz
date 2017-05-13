e04160a7d6b311a50a43f3df7a4ba376c893947a
Steve Haigh <steve.haigh@gmail.com>
2017-05-13 18:25:48 +0100

    breaking the build to test travis

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,5 +1,4 @@
 import unittest
-
 from buzz import buzz
 
 def test_sample_single_word():
@@ -18,3 +17,6 @@ def test_sample_multiple_words():
 def test_generate_buzz_of_at_least_five_words():
     phrase = buzz.generate_buzz()
     assert len(phrase.split()) >= 5
+
+def test_break_the_build():
+    assert False
